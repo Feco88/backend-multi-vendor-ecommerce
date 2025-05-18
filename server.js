1ea6280ff167e7fc4cebf13144a4fd441d8ba6f9
Feco88 <fecaboy@gmail.com>
2025-05-18 15:46:05 +0200

    frissites2

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 app.use(cors({
-    origin : process.env.mode === 'pro' ? [process.env.client_customer_production_url,] :
+    origin : process.env.mode === 'pro' ? [process.env.client_customer_production_url,
+    process.env.client_admin_production_url] :
     ['http://localhost:3000', 'http://localhost:3001'],
     credentials : true
 }))
 //socket szerver beállítás
 const io = socket(server, {
     cors: {
-    origin : process.env.mode === 'pro' ? [process.env.client_customer_production_url,] :
+    origin : process.env.mode === 'pro' ? [process.env.client_customer_production_url,
+    process.env.client_admin_production_url] :
     ['http://localhost:3000', 'http://localhost:3001'],
     //origin: '*', //összes routes
     credentials: true
